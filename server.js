f7a6bacee12eb847712465845e4d434816dffaef
Louis Kim <louiskim@Louiss-MacBook-Pro.local>
2019-08-14 22:21:01 -0700

    functional as fuck

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Readable = require('stream').Readable;
+const markdownpdf = require('markdown-pdf');
+
 const app = express();
 
+app.use(bodyParser.urlencoded ({
+    extended: true
+}))
 app.set('view engine', 'ejs')
 
 // Route
@@ -10,7 +17,15 @@ app.get('/', function(req, res){
 
 // route for post
 app.post('/pdf', function (req,res) {
-
+    const stream = new Readable();
+    stream._read = () => {};
+    var MD = req.body.MD;
+    res.set('Content-Disposition', 'attachment');
+    res.set('Content-type', 'application/pdf');
+    stream.pipe(markdownpdf())
+        .pipe(res);
+    stream.push(MD);
+    stream.push(null);
 })
 
 app.listen(1337)
